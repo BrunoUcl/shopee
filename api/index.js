2c93df9ee66be2a17635e912f227620d6753d305
BrunoUcl <25967804+BrunoUcl@users.noreply.github.com>
2025-07-30 05:04:00 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api/index.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.redirect(302, 'https://script.google.com/macros/s/AKfycbzmsJh2-mOA-OmjABG9atz3O4NuCLGVKKeq23cjWIqH2jaUugx2VRxXqS9sDE0O3q9BRA/exec');
+}
